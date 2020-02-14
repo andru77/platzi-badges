3bf24468f957504ad02967ba6c06beda94da9080
Jose Colmenares <andrescolmenares72@gmail.com>
2020-02-13 20:53:46 -0400

    playing with jsx

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from 'react';   /*Import in all react js's*/
 import ReactDOM from 'react-dom';  /*Import when you have to edit DOM from the your Js files*/
 
 
-const el = <h1>Hello world platzi!</h1>;  // This only works beacuase of the react import 
+const jsx = (
+    <div>
+        <h1 class="red font">This is the first child</h1>
+        <p>This is a child</p>
+    </div>
+
+);  // This only works beacuase of the react import  JSX 
+
+// const element  = React.createElement('h1', {},`Hey I am testing shits ${name}`)  // the uggly way using react
 
 const container = document.getElementById('app');
 
-ReactDOM.render(el,container);
+ReactDOM.render(jsx ,container);
 
